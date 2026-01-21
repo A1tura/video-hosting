6c9421d4c64a12c277b3ba4f90e9e2245b39ffdc
Your Name <you@example.com>
2026-01-21 14:22:26 +0100

    chenged fetch

--- frontend/src/statics/js/main.js
+++ frontend/src/statics/js/main.js
@@ -2,7 +2,7 @@ const videoElement = document.getElementById("videos");
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://127.0.0.1:8081/videos").then((res) => res.json()).then((data) => {
+    fetch("http://0.0.0.1:8081/videos").then((res) => res.json()).then((data) => {
         for (let i = 0; i < data.length; i++) {
             const element = document.createElement("a")
             element.textContent = data[i].title
